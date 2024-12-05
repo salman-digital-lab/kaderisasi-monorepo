cae3097a76d59599ea73352f749c352d92e61b05
Naufal Zhafran Latif <latifnaufal7@gmail.com>
2024-12-05 15:38:47 +0700

    feat: add monorepo code

---
+++ packages/data-model/index.ts
@@ -0,0 +1,11 @@
+export type * from "./model/activity";
+export type * from "./model/adminuser";
+export type * from "./model/city";
+export type * from "./model/members";
+export type * from "./model/province";
+export type * from "./model/ruangcurhat";
+export type * from "./model/university";
+
+export * from "./constants/activity";
+export * from "./constants/profile";
+export * from "./constants/ruangcurhat";
