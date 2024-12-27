c9f0671bb17415671b7406dd9f8ef063623c34ee
Naufal Zhafran Latif <latifnaufal7@gmail.com>
2024-12-27 15:32:34 +0700

    chore: add disable config for web fe

--- apps/web-fe/next.config.mjs
+++ apps/web-fe/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
+    missingSuspenseWithCSRBailout: false,
     optimizePackageImports: [
       "@mantine/core",
       "@mantine/hooks",
